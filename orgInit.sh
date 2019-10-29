d58612d1446c67f11c157fa56ca3f77272075c8c
mshanemc <shane.mclaughlin@salesforce.com>
2019-10-29 17:22:45 -0500

    deployable

--- orgInit.sh
+++ orgInit.sh
@@ -1,3 +1,5 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx force:package:install --package 04t1U000007Tj8tQAC -w 20
+sfdx force:package:install --package 04t2v0000019Q9oAAE -w 20
 sfdx force:source:push
 sfdx force:org:open
